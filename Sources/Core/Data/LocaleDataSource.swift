90930bccb1db981f29f4aa7146486e67a6ddc3f5
Zuhrul Anam <zuhrrlanam@gmail.com>
2024-08-20 19:09:20 +0700

    Update LocaleDataSource.swift

--- Sources/Core/Data/LocaleDataSource.swift
+++ Sources/Core/Data/LocaleDataSource.swift
@@ -13,7 +13,7 @@ public protocol LocaleDataSource {
     associatedtype Response
     
     func list(request: Request?) -> AnyPublisher<[Response], Error>
-    func add(entities: Response) -> AnyPublisher<Bool, Error>
+    func add(entitie: Response) -> AnyPublisher<Bool, Error>
     func get(id: Int) -> AnyPublisher<Bool, Error>
     func update(id: Int, entity: Response) -> AnyPublisher<Bool, Error>
 }
